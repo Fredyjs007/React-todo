ce89485b383412f22aff7479e4b7fb8420199134
Fredy Rosales <fredyr78@yahoo.com>
2016-11-23 12:23:12 -0800

    ToDoApp component

--- app/app.jsx
+++ app/app.jsx
@@ -1,6 +1,7 @@
   var React = require('react');
   var ReactDOM = require('react-dom');
   var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+  var TodoApp = require('TodoApp');
 
   // Load foundation
   $(document).foundation();
@@ -9,6 +10,6 @@
   require('style!css!sass!applicationStyles')
 
   ReactDOM.render(
-    <p>Boiler Plate 3</p>,
+    <TodoApp/>,
     document.getElementById('app')
   );
