ac4f22bed04abc882bca624412b4e03b40bad56b
Fredy Rosales <fredyr78@yahoo.com>
2016-12-02 12:29:42 -0800

    Refactor app to work with local storage

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -46,6 +46,11 @@
             return todo;
           }
         });
+      case 'ADD_TODOS':
+        return [
+          ...state,
+          ...action.todos
+        ];
       default:
         return state;
     };
